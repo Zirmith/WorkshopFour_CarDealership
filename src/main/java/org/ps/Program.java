f726c76efc39bb7aac86fae5bb28ef6e8cf69dc2
Zirmith <f2v74yt@gmail.com>
2024-10-24 14:46:15 -0500

    first commit

---
+++ src/main/java/org/ps/Program.java
@@ -0,0 +1,7 @@
+package org.ps;
+
+public class Program {
+    public static void main(String[] args) {
+       UserInterface.display();
+    }
+}
---
+++ src/main/java/org/ps/UserInterface.java
@@ -0,0 +1,15 @@
+package org.ps;
+
+public class UserInterface {
+    // private static Dealership dealership;
+    public static void init(){
+        System.out.println("Init");
+        // loading dealership and inventory from file
+    }
+
+    public static void display(){
+        System.out.println("Display");
+
+        init(); // load menu
+    }
+}
